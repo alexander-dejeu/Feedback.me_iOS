3e28960f5fcf8d444bc451b75aad5d973a7f0b4b
Alexander Dejeu <alexdejeu@gmail.com>
2017-01-13 21:40:32 -0800

    Added the basic functionality for the classrooms tab

---
+++ Feedback.me/Feedback.me/Custom Elements/ClassCell.swift
@@ -0,0 +1,19 @@
+//
+//  ClassCell.swift
+//  Feedback.me
+//
+//  Created by Alexander Dejeu on 1/13/17.
+//  Copyright © 2017 Do Good Technology. All rights reserved.
+//
+
+import UIKit
+
+class ClassCell: UITableViewCell {
+    //MARK: - IBOutlets
+    @IBOutlet weak var cellBackgroundView : UIView!
+    @IBOutlet weak var cellColorHighlight : UIView!
+//    @IBOutlet weak var extraDetailsView : UIView!
+    @IBOutlet weak var classTitle : UILabel!
+    @IBOutlet weak var classGrade : UILabel!
+
+}
---
+++ Feedback.me/Feedback.me/View Controllers/ClassesController.swift
@@ -0,0 +1,33 @@
+//
+//  ClassesController.swift
+//  Feedback.me
+//
+//  Created by Alexander Dejeu on 1/13/17.
+//  Copyright © 2017 Do Good Technology. All rights reserved.
+//
+
+import UIKit
+
+class ClassesController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    //MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    //MARK: - TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "classCell", for: indexPath) as! ClassCell
+        return cell
+    }
+    
+    // MARK: - Navigation
+
+
+}
