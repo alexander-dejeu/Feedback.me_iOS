9d0297e830212a3b400f35eff5bb58e946f4121e
Alexander Dejeu <alexdejeu@gmail.com>
2017-01-14 16:13:36 -0800

    added star rating modular functionality

--- Feedback.me/Feedback.me/Custom Elements/StarRatingView.swift
+++ Feedback.me/Feedback.me/Custom Elements/StarRatingView.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class StarRatingView: UIView {
 
-
+    var starWidth : CGFloat = 17.0
+    var starHeight: CGFloat = 16.0
+    var starSpacing : CGFloat = 2.0
+    
+    func addStars(rating: Int){
+        clearStars()
+    
+        starSpacing = (self.frame.width - (starWidth * 5)) / 4.0
+        
+        for i in 0..<rating{
+            let star = UIImage(named: "filledStar")
+            let starFrame = CGRect(x: CGFloat(i) * (starWidth + starSpacing), y: 0, width: starWidth, height: starHeight)
+            let starView = UIImageView(frame: starFrame)
+            starView.image = star
+            
+            self.addSubview(starView)
+        }
+    }
+    
+    func clearStars(){
+        for view in self.subviews{
+            view.removeFromSuperview()
+        }
+    }
 
 }
