adeb4a3d7be4f5694a1a7bc6ae7ea4acfd090bb0
Alexander Dejeu <alexdejeu@gmail.com>
2017-01-16 15:08:44 -0800

    mad progress for the navigation, tap gestures and other naviances

--- Feedback.me/Feedback.me/View Controllers/FormDetailController.swift
+++ Feedback.me/Feedback.me/View Controllers/FormDetailController.swift
@@ -9,13 +9,62 @@
 import UIKit
 
 class FormDetailController: UIViewController {
-
+    //MARK: - IBOutlets
+    @IBOutlet weak var teacherImageView : UIImageView!
+    @IBOutlet weak var teacherNameLabel : UILabel!
+    @IBOutlet weak var classLabel : UILabel!
+    @IBOutlet weak var sentLabel : UILabel!
+    @IBOutlet weak var selfAssessementView : UIView!
+    @IBOutlet weak var peerAssesementView : UIView!
+    @IBOutlet weak var teacherAssesementView : UIView!
+    @IBOutlet weak var selfLabel : UILabel!
+    @IBOutlet weak var peerLabel : UILabel!
+    @IBOutlet weak var teacherLabel : UILabel!
+    @IBOutlet weak var scrollView : UIScrollView!
     
-    
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
+        styleCards()
+        
+//        scrollView.backgroundColor = globalColors.background
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.tapCard(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.cancelsTouchesInView = false
+        tapGesture.isEnabled = true
 
+        let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(self.tapCard(_:)))
+        tapGesture2.numberOfTapsRequired = 1
+        tapGesture2.cancelsTouchesInView = false
+        tapGesture2.isEnabled = true
+        
+        let tapGesture3 = UITapGestureRecognizer(target: self, action: #selector(self.tapCard(_:)))
+        tapGesture3.numberOfTapsRequired = 1
+        tapGesture3.cancelsTouchesInView = false
+        tapGesture3.isEnabled = true
+        
+        selfAssessementView.addGestureRecognizer(tapGesture)
+        peerAssesementView.addGestureRecognizer(tapGesture2)
+        teacherAssesementView.addGestureRecognizer(tapGesture3)
+    }
+    
     // MARK: - Navigation
+    func tapCard(_ sender: UITapGestureRecognizer){
+        print("pls jappe")
+        self.performSegue(withIdentifier: "segueFromFormDetailsToQuestions", sender: nil)
+    }
+    
+    
+    //MARK: - Helpers
+    func styleCards(){
+        self.selfAssessementView.layer.borderColor = globalColors.boarders.cgColor
+        self.selfAssessementView.layer.borderWidth = 1
+        
+        self.peerAssesementView.layer.borderColor = globalColors.boarders.cgColor
+        self.peerAssesementView.layer.borderWidth = 1
+        
+        self.teacherAssesementView.layer.borderColor = globalColors.boarders.cgColor
+        self.teacherAssesementView.layer.borderWidth = 1
+    }
 
 }
--- Feedback.me/Feedback.me/View Controllers/FormsHomeController.swift
+++ Feedback.me/Feedback.me/View Controllers/FormsHomeController.swift
@@ -22,3 +22,27 @@ class FormsHomeController: UIViewController {
     // MARK: - Navigation
 
 }
+
+extension FormsHomeController : UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "SegueFromFormsHomeToFormDetail", sender: nil)
+    }
+}
+
+extension FormsHomeController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let cellWidth = (self.view.frame.width / 2) - 12
+        let cellHeight = cellWidth
+        print(cellWidth)
+        return CGSize(width: cellWidth, height: cellHeight);
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "formCell", for: indexPath as IndexPath) as! FormCollectionCell
+        return cell
+    }
+
+}
